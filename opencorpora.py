7b553a5de0b27121dab5e307f7783b6198deb012
Vladimir Berlev <bvispb@gmail.com>
2017-04-12 20:27:30 +0200

    Develop opencorpora parsing

--- opencorpora.py
+++ opencorpora.py
@@ -11,8 +11,8 @@ import logging
 log = logging.getLogger(__name__)
 
 def parse(path='../dict.opcorpora.txt', misses=None):
-    res = []
-    re = regex.compile('^(?P<W>.+)\t+(?P<S>[\w-,]+)( (?P<F>[\w-,]+))? ?$')
+    res = {}
+    re = regex.compile('^(?P<W>.+(?=\t))\t+(?P<S>[\w-,]+)( (?P<F>[\w-,]+))? ?$')
     with open(path) as f:
         for line in f.readlines():
             match = re.match(line)
@@ -20,6 +20,10 @@ def parse(path='../dict.opcorpora.txt', misses=None):
                 pass # TODO: add some handling
             else:
                 grammems = grmodel.get_grammems(match, misses)[0]
+                if grmodel.Abbr.__name__ in grammems.keys():
+                    continue
                 lemma = match.group('W').lower()
-                print(lemma)
+                if lemma not in res.keys():
+                    res[lemma] = []
+                res[lemma].append(grammems)
     return res
