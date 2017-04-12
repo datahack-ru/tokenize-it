00b53050c0aefc163da33af148fea913f01825c4
Vladimir Berlev <bvispb@gmail.com>
2017-04-12 20:28:19 +0200

    Implement naive dictionary lookup based tokenization

---
+++ tok.py
@@ -0,0 +1,37 @@
+from xml.etree import ElementTree as ET
+from os import listdir as ls
+from os.path import isfile, isdir, join
+from fnmatch import fnmatch
+import regex
+import grmodel
+
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class Tok:
+    def __init__(self, dict):
+        self.dict = dict
+
+    def words(self, text):
+        res = []
+        for i in range(1, len(text) + 1):
+            token = text[:i]
+            if token in self.dict.keys():
+                res.append([token, text[i:]])
+        return res
+
+    def tokenize(self, text):
+        text.lower()
+        res = []
+        queue = self.words(text)
+        while queue:
+            seq = queue.pop()
+            tail = seq.pop()
+            if tail:
+                for s in self.words(tail):
+                    queue = queue + [seq + s]
+            else:
+                res.append(seq)
+        return res
